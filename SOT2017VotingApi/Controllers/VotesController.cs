5179ceaf8eeae0d6c620008166d92098c87658b8
Samuel Debruyn <samuel.debruyn@3factr.be>
2017-05-28 20:21:10 +0200

    add dotnet core api and vote controller

---
+++ SOT2017VotingApi/Controllers/VotesController.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SOT2017VotingApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class VotesController : Controller
+    {
+        [HttpGet]
+        public IReadOnlyDictionary<string, int> Get() => VoteStorage.Instance.GetVotes();
+        
+        [HttpPost]
+        public void Post([FromBody] string value) => VoteStorage.Instance.Add(value);
+    }
+}
---
+++ SOT2017VotingApi/VoteStorage.cs
@@ -0,0 +1,54 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+
+namespace SOT2017VotingApi
+{
+    public class VoteStorage
+    {
+        static volatile VoteStorage _instance;
+        static readonly object LockObject = new object();
+        
+        public static VoteStorage Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (LockObject)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new VoteStorage();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        readonly ConcurrentDictionary<string, int> _votes;
+
+        private VoteStorage()
+        {
+            _votes = new ConcurrentDictionary<string, int>();
+        }
+
+        public void Add(string voteOption)
+        {
+            if (string.IsNullOrEmpty(voteOption))
+            {
+                return;
+            }
+            
+            _votes.AddOrUpdate(voteOption.Trim().ToLower(), 1, (opt, voteCount) => voteCount + 1);
+        }
+
+        public IReadOnlyDictionary<string, int> GetVotes()
+        {
+            return new SortedDictionary<string, int>(_votes);
+        }
+    }
+}
